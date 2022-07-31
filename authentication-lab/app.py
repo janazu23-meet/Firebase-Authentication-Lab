5a1fdd11d2ab3c44d9f0861623c38e32991c0c5e
janazu23-meet <109899957+janazu23-meet@users.noreply.github.com>
2022-07-31 14:31:42 +0300

    completed without bonus

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -8,18 +8,55 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    try:
+        login_session['user'] = 
+            auth.sign_in_with_email_and_password(email, password)
+        return redirect(url_for('add_tweet'))
+    except:
+        error = "Authentication failed"
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    try:
+        login_session['user'] = 
+            auth.create_user_with_email_and_password(email, password)
+        return redirect(url_for('add_tweet'))
+    except:
+        error = "Authentication failed"
     return render_template("signup.html")
 
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
 
+firebaseConfig = {
+  "apiKey": "AIzaSyCE4jciSJDWWcSJORsd8GPBS_kmRCKCcU4",
+  "authDomain": "fir-lab-ecc1d.firebaseapp.com",
+  "projectId": "fir-lab-ecc1d",
+  "storageBucket": "fir-lab-ecc1d.appspot.com",
+  "messagingSenderId": "591618864518",
+  "appId": "1:591618864518:web:d961eb5e55ec8cf7bb2807",
+  "measurementId": "G-KQGQTHRZYB",
+  "databaseURL": "" }
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 if __name__ == '__main__':
     app.run(debug=True)
